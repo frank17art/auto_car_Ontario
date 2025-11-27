6f87857d1babf0260d20b9932402ce7867a20b58
aymenToronto <aymenharkati110@gmail.com>
2025-11-27 14:37:53 -0500

    étape 2

--- auto_car/lib/widgets/contact_form.dart
+++ auto_car/lib/widgets/contact_form.dart
@@ -19,3 +19,30 @@ class ContactForm extends StatefulWidget {
   @override
   State<ContactForm> createState() => _ContactFormState();
 }
+class _ContactFormState extends State<ContactForm> {
+  late final TextEditingController _firstNameController;
+  late final TextEditingController _lastNameController;
+  late final TextEditingController _emailController;
+  late final TextEditingController _phoneController;
+  late final TextEditingController _messageController;
+  
+  bool _isLoading = false;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _firstNameController = TextEditingController(
+      text: widget.initialFirstName ?? '',
+    );
+    _lastNameController = TextEditingController(
+      text: widget.initialLastName ?? '',
+    );
+    _emailController = TextEditingController(
+      text: widget.initialEmail ?? '',
+    );
+    _phoneController = TextEditingController(
+      text: widget.initialPhone ?? '',
+    );
+    _messageController = TextEditingController();
+  }
